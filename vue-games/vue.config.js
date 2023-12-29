1ff5dfec0354c49a1a541c33c517b2df3af5cb20
Nita Shutt <nita.madison@live.com>
2023-12-28 18:29:11 -0700

    configured vue

--- vue-games/vue.config.js
+++ vue-games/vue.config.js
@@ -1,4 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  publicPath: 'http://localhost:8080',
+  outputDir: '../static/dist',
+  indexPath: '../../templates/_base_vue.html',
+
+  configureWebpack: {
+    devServer: {
+      devMiddleware: {
+        writeToDisk: true
+      }
+    }
+  }
+};
